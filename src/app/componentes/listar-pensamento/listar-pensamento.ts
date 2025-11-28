44c4b5f6b87a341824e4e95cc1588fd409e38635
Ricardo Cossa <ricardoccossa@gmail.com>
2025-11-28 12:41:12 +0200

    for no angular

--- src/app/componentes/listar-pensamento/listar-pensamento.ts
+++ src/app/componentes/listar-pensamento/listar-pensamento.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from "@angular/router";
+import { Pensamento } from "../pensamento/pensamento";
 
 @Component({
   selector: 'app-listar-pensamento',
-  imports: [RouterLink],
+  imports: [RouterLink, Pensamento],
   templateUrl: './listar-pensamento.html',
   styleUrl: './listar-pensamento.css',
 })
 export class ListarPensamento {
-
+  listarPensamento = [];
 }
